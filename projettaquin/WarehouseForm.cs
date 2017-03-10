f3861f32b4df5f157486d52ee1afe6f0cc6bea3f
Jean Basset <jbasset001@ensc.fr>
2017-03-10 11:33:52 +0100

    Aubin, your play

---
+++ projettaquin/WarehouseForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projettaquin
+{
+    public partial class WarehouseForm : Form
+    {
+        static Graphics g;
+        static Pen pen;
+
+        public WarehouseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WarehouseForm_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            pen = new Pen(Color.Chartreuse,15);
+        }
+
+        private void WarehouseForm_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 24; j++)
+                {
+                    g.DrawRectangle(pen, i * 15, j * 15, 15, 15);
+                }
+            }
+        }
+    }
+}
